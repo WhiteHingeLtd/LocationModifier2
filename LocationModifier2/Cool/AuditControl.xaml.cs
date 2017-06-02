6cc4a2aa1d3f47b8637319bc59a8aee13f88b815
Owen <Floul1@hotmail.co.uk>
2017-06-02 11:00:28 +0100

    Fixed an SQL error

--- LocationModifier2/Cool/AuditControl.xaml.cs
+++ LocationModifier2/Cool/AuditControl.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using WHLClasses;
 
 namespace LocationModifier2.Cool
 {
@@ -24,7 +12,7 @@ namespace LocationModifier2.Cool
         public AuditControl(Dictionary<string,object> data)
         {
             InitializeComponent();
-            AdjustText.Text = auditstrings[(data["AuditEvent"] as int?).Value] + " " + data["additional"].ToString();
+            AdjustText.Text = auditstrings[(data["AuditEvent"] as int?).Value] + " " + data["stock"].ToString();
             DetailText.Text = (data["DateRecorded"] as DateTime?).Value.ToString("dd/MM/yyyy HH:mm:ss") + " - User " +
                               data["AuditUserID"].ToString() + " on " + data["packsize"].ToString() + " pack at " +
                               data["loctext"].ToString();
